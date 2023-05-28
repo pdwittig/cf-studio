c6dedd0be3b510f12ec6527ba9c63ac04b5fa7e2
Philly <pdwittig@gmail.com>
2023-05-28 13:00:29 -0700

    add basic beat play functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+const TONES = [
+  { label: '01', path: `${process.env.PUBLIC_URL}/tone/kick.ogg` },
+  { label: '02', path: `${process.env.PUBLIC_URL}/tone/clap.ogg` },
+  { label: '03', path: `${process.env.PUBLIC_URL}/tone/closed-hat.ogg` },
+  { label: '04', path: `${process.env.PUBLIC_URL}/tone/open-hat.ogg` },
+]
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex mt-20">
+      <div className="grid grid-cols-4 gap-4 mx-auto">
+        {
+          TONES.map((tone, index) => <Tone tone={tone} key={index} />)
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+function Tone({ tone, ...restProps }) {
+
+  const audio = new Audio(tone.path)
+  const play = () => {
+    audio.pause();
+    audio.currentTime = 0;  
+    audio.play();
+  }
+
+  return (
+    <div>
+     <button
+        key={restProps.key}
+        className="p-5 bg-gray-100 hover:bg-gray-200 focus:outline-none active:outline-none rounded-md"
+        onClick={play}
+      >
+        {tone.label}
+      </button>
+    </div>
+  )
+}
